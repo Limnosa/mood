db99ac0f5241406785773184eb26da5d79412cee
Limnosa <bogiblabla@gmail.com>
2024-09-26 14:22:07 +0200

    change style

--- static/js/script.js
+++ static/js/script.js
@@ -1,50 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-//setmood-page appears
-    window.addEventListener('load', function() {
-        document.body.classList.add('visible');
-    });
-//login-page animation
-    const loginForm = document.getElementById('loginForm');
-    if(loginForm) {
-        const handleSubmit = function(event) {
-            event.preventDefault();
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', '/', true);
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    const response = JSON.parse(xhr.responseText);
-                    if (response.success) {
-                        loginForm.removeEventListener('submit', handleSubmit);
-                        document.querySelector('.index-waves').classList.toggle('anim-trans-w');
-                        document.querySelector('.wave-background').classList.toggle('anim-trans-bg');
-                        localStorage.setItem('loginSuccess', 'true');
-                        setTimeout(function() {
-                            window.location.href = '/setmood';
-                        }, 1000);
-                    } else {
-                        const errorDiv = document.querySelector('.error');
-                        if (errorDiv) {
-                            console.log('Error div found:', errorDiv);
-                            errorDiv.textContent = response.message;
-                        }
-                    }
-                }
-            };
-            const formData = new FormData(loginForm);
-            xhr.send(formData);
-            };
-        loginForm.addEventListener('submit', handleSubmit);
-    };
-
-//setmood-page succes login animation
-    const loginSuccess = localStorage.getItem('loginSuccess');
-    if (loginSuccess) {
-        document.querySelector('.li-wave-bg').classList.toggle('anim-trans-li-bg');
-        document.querySelector('.li-header .waves').classList.toggle('anim-trans-li');
-        localStorage.removeItem('loginSuccess');
-    }
-
 //setmood-page stars animation
     const stars = document.querySelectorAll('.rate-radio');
     const smiley = document.getElementById('smiley');
